eecdfdf8adaa10b5d3a48db9b30a32e830120d6f
Eugene Sillona <93899090+jojiSillona@users.noreply.github.com>
2022-10-10 23:15:49 +0800

    FLOWCHART finalizations

--- javascript/flowchart.js
+++ javascript/flowchart.js
@@ -3,3 +3,30 @@ $(".subject").draggable({
     cursor: "grabbing",
     grid: [110, 60]
 });
+
+//CREATING LINES LOLOLOL
+
+const svgCanvas = document.getElementById("svg-canvas");
+
+let target = document.querySelectorAll('.subject');
+var sender, receiver;
+for(var i = 0; i < target.length; i++){
+    if(target[i].textContent === "CCPROG1"){
+        sender = target[i];
+    }
+    if(target[i].textContent === "CCPROG2"){
+        receiver = target[i];
+    }
+}
+
+let senderProperties = sender.getBoundingClientRect();
+let receiverProperties = sender.getBoundingClientRect();
+
+let bezierControlX = ( senderProperties.x + receiverProperties.x)/ 2;
+
+/*
+var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+path.setAttribute("d", `M ${}`)
+*/
+
+//TODO: set the properties of sender and receiver to path wahehe
