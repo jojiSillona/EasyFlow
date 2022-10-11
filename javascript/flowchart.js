6d0d970723942fe12273aad24f7ed7c3d65d081e
Eugene Sillona <93899090+jojiSillona@users.noreply.github.com>
2022-10-11 14:04:57 +0800

    Line formation added

--- javascript/flowchart.js
+++ javascript/flowchart.js
@@ -7,26 +7,44 @@ $(".subject").draggable({
 //CREATING LINES LOLOLOL
 
 const svgCanvas = document.getElementById("svg-canvas");
-
 let target = document.querySelectorAll('.subject');
-var sender, receiver;
-for(var i = 0; i < target.length; i++){
-    if(target[i].textContent === "CCPROG1"){
-        sender = target[i];
+
+function strokeLine(prerequisite, requisite){
+    var sender, receiver;
+    for(var i = 0; i < target.length; i++){
+        if(target[i].textContent === prerequisite){
+            sender = target[i];
+        }
+        if(target[i].textContent === requisite){
+            receiver = target[i];
+        }
     }
-    if(target[i].textContent === "CCPROG2"){
-        receiver = target[i];
-    }
+    
+    let senderProperties = sender.getBoundingClientRect();
+    let receiverProperties = receiver.getBoundingClientRect();
+    
+    let bezierControlX = ( senderProperties.left + 90 + receiverProperties.left)/ 2;
+    
+    //TODO: Try offset-left
+
+    var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", `M ${senderProperties.left + 90 - 214.31}, ${senderProperties.top + 20 - 357.8} 
+                            C ${bezierControlX - 214.31}, ${senderProperties.top + 20 - 357.8}  
+                            ${bezierControlX - 214.31}, ${receiverProperties.top + 20 - 357.8}  
+                            ${receiverProperties.left - 214.31}, ${receiverProperties.top + 20 - 357.8}`);
+    path.setAttribute("stroke", "black");
+    path.setAttribute("fill", "transparent");
+    
+    svgCanvas.appendChild(path);
 }
 
-let senderProperties = sender.getBoundingClientRect();
-let receiverProperties = sender.getBoundingClientRect();
 
-let bezierControlX = ( senderProperties.x + receiverProperties.x)/ 2;
 
-/*
-var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-path.setAttribute("d", `M ${}`)
-*/
+strokeLine("CCPROG1", "CCPROG2");
+strokeLine("CCPROG2", "CCPROG3");
+strokeLine("CCDSTRU", "CCDSALG");
+strokeLine("MTH101A", "CSMATH1");
+strokeLine("MTH101A", "CSMATH2");
 
-//TODO: set the properties of sender and receiver to path wahehe
+
+//TODO: Update line everytime subjects move
