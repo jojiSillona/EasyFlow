51c56ceea94ad2e547b79853c811dfb25c944321
Eugene Sillona <93899090+jojiSillona@users.noreply.github.com>
2022-10-12 15:26:42 +0800

    flowchart debugs

--- javascript/flowchart.js
+++ javascript/flowchart.js
@@ -1,13 +1,14 @@
-$(".subject").draggable({
-    containment: $("#canvas"),
-    cursor: "grabbing",
-    grid: [110, 60]
-});
-
 //CREATING LINES LOLOLOL
-
 const svgCanvas = document.getElementById("svg-canvas");
 let target = document.querySelectorAll('.subject');
+
+function simplifyCoordinates(targetStyle){
+    if(targetStyle === "auto"){
+        return 0;
+    } else {
+        return targetStyle.replace('px', '');
+    }
+}
 
 function strokeLine(prerequisite, requisite){
     var sender, receiver;
@@ -20,25 +21,31 @@ function strokeLine(prerequisite, requisite){
         }
     }
     
-    let senderProperties = sender.getBoundingClientRect();
-    let receiverProperties = receiver.getBoundingClientRect();
-    
-    let bezierControlX = ( senderProperties.left + 90 + receiverProperties.left)/ 2;
-    
-    //TODO: Try offset-left
+    var senderX = window.getComputedStyle(sender).left;
+    senderX = Math.abs(parseInt(simplifyCoordinates(senderX)));
+    var senderY = window.getComputedStyle(sender).top;
+    senderY = Math.abs(parseInt(simplifyCoordinates(senderY)));
+    var receiverX = window.getComputedStyle(receiver).left;
+    receiverX = Math.abs(parseInt(simplifyCoordinates(receiverX)));
+    var receiverY = window.getComputedStyle(receiver).top;
+    receiverY = Math.abs(parseInt(simplifyCoordinates(receiverY)));
+
+    let bezierControlX = (senderX + 110 + receiverX)/ 2;
+
+    console.log(prerequisite + " TO " + requisite + " SENDER: "+ senderX + ", " + senderY + " RECEIVER: " + receiverX + ", " + receiverY + " BEZIER: " + bezierControlX);
 
     var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path.setAttribute("d", `M ${senderProperties.left + 90 - 214.31}, ${senderProperties.top + 20 - 357.8} 
-                            C ${bezierControlX - 214.31}, ${senderProperties.top + 20 - 357.8}  
-                            ${bezierControlX - 214.31}, ${receiverProperties.top + 20 - 357.8}  
-                            ${receiverProperties.left - 214.31}, ${receiverProperties.top + 20 - 357.8}`);
+    path.setAttribute("id", `${prerequisite}-${requisite}`);
+
+    path.setAttribute("d", `M ${senderX + 110}, ${senderY + 40},
+                            C ${bezierControlX}, ${senderY + 40}
+                            ${bezierControlX}, ${receiverY - 20}
+                            ${receiverX + 20}, ${receiverY - 20}`);
     path.setAttribute("stroke", "black");
     path.setAttribute("fill", "transparent");
     
     svgCanvas.appendChild(path);
 }
-
-
 
 strokeLine("CCPROG1", "CCPROG2");
 strokeLine("CCPROG2", "CCPROG3");
@@ -46,5 +53,12 @@ strokeLine("CCDSTRU", "CCDSALG");
 strokeLine("MTH101A", "CSMATH1");
 strokeLine("MTH101A", "CSMATH2");
 
+//TODO: Animate line whenever subjects move
 
-//TODO: Update line everytime subjects move
+var draggedSubject = "CCPROG1"
+
+$(".subject").draggable({
+    containment: $("#canvas"),
+    cursor: "grabbing",
+    grid: [110, 60]
+});
